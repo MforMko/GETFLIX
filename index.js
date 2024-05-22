2862ad09dfb55bc8e166f46247caa6772cad9bed
Mark O'Connor <mko1@me.com>
2024-05-22 13:11:29 +0100

    trying to fix CORS issue
    adding allowed origins for CORS

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ app.use(express.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
